ad45da65981aa9095432634d18cd53716b2db9d7
“Pia <pia.hoelzl@gmx.at>
2024-02-03 21:51:48 +0100

    Add hero img

---
+++ app/header.js
@@ -0,0 +1,12 @@
+import './globals.scss';
+import './header.scss';
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      <img src="/images/sourcandy_blue.jpeg" alt="sour candy blue" />
+      <h1 className="text-on-image">Hallo saure gummis</h1>
+    </div>
+  );
+}
