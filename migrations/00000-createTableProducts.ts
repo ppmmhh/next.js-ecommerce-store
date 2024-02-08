700e7acd4bff15b5fa0df49d5a535304b1621bda
--local <pia.hoelzl@gmx.at>
2024-02-08 15:24:39 +0100

    Add ley file createTableProducts

---
+++ migrations/00000-createTableProducts.ts
@@ -0,0 +1,27 @@
+import { Sql } from 'postgres';
+
+export type Product = {
+  id: number;
+  name: string;
+  origin: string;
+  image: string;
+  price: number;
+  description: string;
+};
+
+export async function up(sql: Sql) {
+  await sql`
+    CREATE TABLE products (
+      id integer PRIMARY key generated always AS identity,
+      name varchar(40) NOT NULL,
+      origin varchar(40) NOT NULL,
+      image varchar(50) NOT NULL,
+      price integer NOT NULL,
+      description varchar NOT NULL
+    )
+  `;
+}
+
+export async function down(sql: Sql) {
+  await sql`DROP TABLE products`;
+}
