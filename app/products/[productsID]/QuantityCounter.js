aa8b7f926cb2c39f9c2cef2ccb0a923e7c4e1656
--local <pia.hoelzl@gmx.at>
2024-02-08 10:47:56 +0100

    Add file to ensure quantity counting

---
+++ app/products/[productsID]/QuantityCounter.js
@@ -0,0 +1,31 @@
+'use client';
+
+import React, { useState } from 'react';
+import { adaptCookie } from './actions.js';
+import styles from './productpage.module.scss';
+
+export default function SetQuantityCounter(props) {
+  const [quantity, setQuantity] = useState(1);
+  return (
+    <div>
+      <form>
+        <select
+          value={quantity}
+          onChange={(event) => setQuantity(event.currentTarget.value)}
+          className={styles.quantity}
+          data-test-id="product-quantity"
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+        <button
+          formAction={async () => await adaptCookie(props.productID, quantity)}
+        >
+          Buy Now
+        </button>
+      </form>
+    </div>
+  );
+}
