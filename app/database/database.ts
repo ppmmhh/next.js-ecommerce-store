dc91fec20aaabeee97a6175cef7f1caa4d6fb55d
--local <pia.hoelzl@gmx.at>
2024-02-08 19:56:35 +0100

    Add database.ts file

---
+++ app/database/database.ts
@@ -0,0 +1,27 @@
+import { cache } from 'react';
+import { Product } from '../../migrations/00000-createTableProducts';
+import { sql } from './connect';
+
+export const getProductsInsecure = cache(async () => {
+  const products = await sql<Product[]>`
+    SELECT
+      *
+    FROM
+      products
+  `;
+
+  return products;
+});
+
+export const getProductInsecure = cache(async (id: number) => {
+  const [product] = await sql<Product[]>`
+    SELECT
+      *
+    FROM
+      products
+    WHERE
+      id = ${id}
+  `;
+
+  return product;
+});
