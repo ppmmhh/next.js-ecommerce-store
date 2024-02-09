492d796445a995e3a1bebf07b79d183a2db69ca5
--local <pia.hoelzl@gmx.at>
2024-02-09 10:18:41 +0100

    Tweak product overview page

--- app/products/page.js
+++ app/products/page.js
@@ -1,36 +1,53 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { getProductsInsecure } from '../../database/database';
+import styles from './page.module.scss';
 
 export const metadata = {
-  title: {
-    default: 'Products',
-  },
-  description: 'Choose from our variety of apples',
+  title: 'Products',
+  description: 'Chose your favorite apple',
 };
 
 export default async function ProductPage() {
   const products = await getProductsInsecure();
 
   return (
-    <>
-      <div>Freshly picked for you:</div>
-      {products.map((product) => {
-        return (
-          <div key={`products-${product.id}`}>
-            <Link href={`/products/${product.id}`}>
-              <h1>{product.name}</h1>
-
-              <Image
-                src={`/images/${product.name}.png`}
-                alt="Alt text"
-                width={400}
-                height={400}
-              />
-            </Link>
-          </div>
-        );
-      })}
-    </>
+    <div className={styles.sectionContainer}>
+      <div>
+        <h1>Freshly picked for you:</h1>
+      </div>
+      <div>
+        <div className={styles.productContainer}>
+          {products.map((product) => {
+            return (
+              <div key={`products-${product.id}`}>
+                <Link
+                  href={`/products/${product.id}`}
+                  data-test-id={`product-${product.id}`}
+                  className={styles.productItem}
+                >
+                  <Image
+                    src={product.image}
+                    width={300}
+                    height={300}
+                    alt={product.name}
+                    className={styles.productItem}
+                  />
+                  <div className={styles.productDetails}>
+                    <div className={styles.headline}>
+                      <h2>{product.name}</h2>
+                    </div>
+                    <div>Origin: {product.origin}</div>
+                    <div data-test-id="product-price">
+                      Price: EUR {product.price}
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
   );
 }
