1909dfb9b88c3b3514808f536681b2808612541d
--local <pia.hoelzl@gmx.at>
2024-02-10 21:41:18 +0100

    Fix typo in migrations file

--- migrations/00000-createTableProducts.ts
+++ migrations/00000-createTableProducts.ts
@@ -14,7 +14,7 @@ export async function up(sql: Sql) {
       id integer PRIMARY key generated always AS identity,
       name varchar(40) NOT NULL,
       origin varchar(40) NOT NULL,
-      price varchar NOT NULL,
+      price varchar(10) NOT NULL,
       description varchar NOT NULL
     )
   `;
