4e5d0cdc4d7107e5c32020d26c1c529c4ecb0525
--local <pia.hoelzl@gmx.at>
2024-02-10 21:40:39 +0100

    Update Products Page

--- app/products/page.js
+++ app/products/page.js
@@ -4,36 +4,37 @@ import { getProductsInsecure } from '../../database/products';
 import styles from './page.module.scss';
 
 export const metadata = {
-  title: {
-    default: 'Products',
-  },
+  title: 'Products',
   description: 'Choose from our variety of products',
 };
 
-export default async function ProductPage() {
-  // const products = getProducts();
-
+export default async function ProductsPage() {
   const products = await getProductsInsecure();
 
   return (
-    <>
-      <div>Freshly picked for you</div>
-      {products.map((product) => {
-        return (
-          <div key={`products-${product.id}`}>
-            <Link href={`/products/${product.id}`}>
-              <h1>{product.name}</h1>
-
+    <div className={styles.sectionContainer}>
+      <div>
+        <h1>Freshly picked for you</h1>
+      </div>
+      <div className={styles.productContainer}>
+        {products.map((product) => (
+          <div key={`product-${product.id}`} className={styles.productItem}>
+            <Link
+              href={`/products/${product.id}`}
+              data-test-id={`product-${product.id}`}
+              className={styles.productItem}
+            >
               <Image
-                src={`/images/${product.name}.jpeg`}
-                alt="product"
+                src={product.image}
                 width={400}
                 height={400}
+                alt={product.name}
+                className={styles.productImage}
               />
             </Link>
           </div>
-        );
-      })}
-    </>
+        ))}
+      </div>
+    </div>
   );
 }
