68a7bfa067d18e9534a3d3efc71c3b3e30ce19e3
--local <pia.hoelzl@gmx.at>
2024-02-13 21:07:51 +0100

    Add more options to quantity counter on product page

--- app/products/[productsID]/QuantityCounter.js
+++ app/products/[productsID]/QuantityCounter.js
@@ -19,8 +19,15 @@ export default function SetQuantityCounter(props) {
           <option value="2">2</option>
           <option value="3">3</option>
           <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
         </select>
         <button
+          className={styles.button}
           formAction={async () => await addToCart(props.productID, quantity)}
         >
           Buy Now
