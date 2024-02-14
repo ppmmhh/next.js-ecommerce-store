0156e7aa23bda605622cbb4aa128aecd6d38e236
--local <pia.hoelzl@gmx.at>
2024-02-14 18:44:44 +0100

    Fix typo

--- database/products.ts
+++ database/products.ts
@@ -2,14 +2,6 @@ import 'server-only';
 import { cache } from 'react';
 import { sql } from './connect';
 
-type Product = {
-  id: number;
-  name: string | null;
-  origin: string | null;
-  price: string | null;
-  description: string | null;
-};
-
 export const getProductsInsecure = cache(async () => {
   const products = await sql<
     {
