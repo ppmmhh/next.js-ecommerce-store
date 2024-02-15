8c846f7080a7e9cbc847a9d49e3e1d7ae5803d87
--local <pia.hoelzl@gmx.at>
2024-02-15 21:33:13 +0100

    Rewrite products.ts

--- database/products.ts
+++ database/products.ts
@@ -2,42 +2,33 @@ import 'server-only';
 import { cache } from 'react';
 import { sql } from './connect';
 
+type Product = {
+  id: number;
+  name: string;
+  origin: string;
+  price: string;
+  description: string | null;
+};
+
+// get all products
 export const getProductsInsecure = cache(async () => {
-  const products = await sql<
-    {
-      id: number;
-      name: string;
-      origin: string;
-      image: string;
-      price: number;
-      description: string;
-    }[]
-  >`
+  const products = await sql<Product[]>`
     SELECT
       *
     FROM
       products
   `;
-
   return products;
 });
 
-export const getProductInsecure = cache(async () => {
-  const [product] = await sql<
-    {
-      id: number;
-      name: string;
-      origin: string;
-      image: string;
-      price: number;
-      description: string;
-    }[]
-  >`
+export const getProductInsecure = cache(async (id: number) => {
+  const [product] = await sql<Product[]>`
     SELECT
       *
     FROM
       products
+    WHERE
+      id = ${id}
   `;
-
   return product;
 });
