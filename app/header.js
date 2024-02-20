a3d29dcae8e5d4a838a1966a9216dc4f376d9d7a
--local <pia.hoelzl@gmx.at>
2024-02-20 22:24:21 +0100

    Remove slideshow

--- app/header.js
+++ app/header.js
@@ -5,23 +5,7 @@ import React from 'react';
 export default function Header() {
   return (
     <div>
-      <div className="simpleSlides">
-        <img
-          className="slides animate-left"
-          src="/images/apple02.jpeg"
-          alt="apples"
-        />
-        <img
-          className="slides animate-left"
-          src="/images/apple01.webp"
-          alt="apples in vase"
-        />
-        <img
-          className="slides animate-left"
-          src="/images/apple03.jpeg"
-          alt="green apple"
-        />
-      </div>
+      <img className="heroimages" src="/images/apple02.jpeg" alt="apples" />
       <div>
         <h1 className="text-on-image">
           there's gonna be a slideshow (eventually)
